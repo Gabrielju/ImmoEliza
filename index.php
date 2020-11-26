898998fb31b606483a398455c2d1476430c2f2c6
Gabrielju <gabrieljulien0804@gmail.com>
2020-11-26 16:36:13 +0100

    test01

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ curl_close($ch);
         <div class="row">
             <!-- LOCATION GOOGLE MAP -->
             <div class="col-12 col-sm-12 col-lg-6 mb-6" id="cardMap">
-                MAPehkdjnlqjlqejnejnlrjni
+                MAP
             </div>
             <!-- SPACE -->
             <div class="col-lg-2 mb-2">
